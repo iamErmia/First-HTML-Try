eec79dcb6adf2560f02f62cde3d944933a0bee42
iamErmia <iam.ermia@yahoo.com>
2024-03-03 13:23:46 +0330

    first-commit

---
+++ JS/Script.js
@@ -0,0 +1,14 @@
+var firstnumber = +prompt("Please enter the first number: ", "12")
+var secondnumber = +prompt("Please enter the Second number: ", "4")
+
+var sum = firstnumber + secondnumber
+var minus = firstnumber - secondnumber
+var multiply = firstnumber * secondnumber
+var divide = firstnumber / secondnumber
+var mod = firstnumber % secondnumber
+
+alert("The summation result is :" + sum)
+alert("The minus result is :" + minus)
+alert("The mulitiplication result is :" + multiply)
+alert("The division result is :" + divide)
+alert("The mod result is :" + mod)
